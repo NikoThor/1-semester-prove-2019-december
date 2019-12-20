518ad32f284cfb1a1266f6a51bc3c1c56a2b33a1
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-12-20 12:31:39 +0100

    Opgave 5; mangler eksempel på Typecasting

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -8,20 +10,20 @@
 public class Person {
 
     // TODO Ret nedenstående datatyper
-    String navn; // Test Testesen
-    String alder; // 18
-    String by; // Næstved
-    String postNummer; // 4700
-    String vej; // Femøvej
-    String husnummer; // 3B
-    String nationalitet; // Dansk
-    String fritidsStringeresser; // Snitning, LOL, strikke
-    String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
-    String hojde; // 1.56 m
-    String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
-    String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
-    String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
+    String navn = "Test Testesen";
+    byte alder = 18;
+    String by = "Næstved";
+    int postNummer = 4700;
+    String vej = "Femøvej";
+    String husnummer = "3B";
+    String nationalitet = "Dansk";
+    String fritidsStringeresser[] = {"Snitning, LOL, strikke"};
+    String smarthomeDevices[] = {"Xbox, 3 Smartlamper, Køleskab m.m."};
+    double hojde = 1.56; //i meter
+    int vaegt = 76; //i kg
+    double karaktergennemsnit = 5.33;
+    int eksamensKarakterer[] = {7, 12, -3, 4, 2, 10};
+    String opdateretDato = "Sun Jan 08 00:00:00 CET 1978";
 
     // TODO Vis eksempler på typecasting
     @Override
